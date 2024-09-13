4280016ae36db66a4408b7037b978873818c33e3
Amal Nasr <nasr.amal88@gmail.com>
2024-09-13 17:38:12 +0200

    ajouter token dans les requetes get (swagger)

--- Back-end/server/routes/userRoutes.js
+++ Back-end/server/routes/userRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 const tokenValidation = require('../middleware/tokenValidation')
 
+
+
 router.post('/signup', userController.createUser)
 
 router.post('/login', userController.loginUser)
@@ -18,5 +20,40 @@ router.put(
   tokenValidation.validateToken,
   userController.updateUserProfile
 )
+
+// // Get all user accounts (secured by JWT token)
+// router.get(
+//   '/accounts',
+//   tokenValidation.validateToken,
+//   userController.getUserAccounts
+// );
+
+// // Get all transactions for an account (secured by JWT token)
+// router.get(
+//   '/accounts/:accountId/transactions',
+//   tokenValidation.validateToken,
+//   transactionController.getTransactionsForAccount
+// );
+
+// // Get a single transaction's details (secured by JWT token)
+// router.get(
+//   '/accounts/:accountId/transactions/:transactionId',
+//   tokenValidation.validateToken,
+//   transactionController.getTransactionDetails
+// );
+
+// // Update a transaction's category (secured by JWT token)
+// router.patch(
+//   '/accounts/:accountId/transactions/:transactionId/category',
+//   tokenValidation.validateToken,
+//   transactionController.updateTransactionCategory
+// );
+
+// // Update a transaction's notes (secured by JWT token)
+// router.patch(
+//   '/accounts/:accountId/transactions/:transactionId/notes',
+//   tokenValidation.validateToken,
+//   transactionController.updateTransactionNotes
+// );
 
 module.exports = router
